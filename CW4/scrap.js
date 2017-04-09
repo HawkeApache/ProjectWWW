ebccd6fabf889d9a6ae2864f0fab96be31ff2062
AdrianLato <adrian.lato@student.uj.edu.pl>
2017-04-09 18:58:02 +0200

    add /movie get method

--- CW4/scrap.js
+++ CW4/scrap.js
@@ -1,47 +1,109 @@
 var fs = require('fs');
+var http = require('http');
+var url = require('url');
 var request = require('request');
 var cheerio = require('cheerio');
 
-url = 'http://www.imdb.com/title/tt0050083/?ref_=nv_sr_1';
+function getMovie(req, res) {
+    var json = '';
 
-request(url, function (error, response, html) {
-    if (!error) {
-        var $ = cheerio.load(html); //sparsowany html w drzewo
+    req.on('data', function (data) {
+        json += data;
+        if (json.length > 1e6)
+            request.connection.destroy();
+    });
 
-        var title, release, rating;
-        var json = {title: "", release: "", rating: ""};
+    req.on('end', function () {
+        var url = JSON.parse(json).URL;
+        request(url, function (error, response, html) {
+            if (!error) {
+                var $ = cheerio.load(html); //sparsowany html w drzewo
 
-        //plik oknfiguracyjny json
-        /*{
-            "titel": "title rwapper h1 ... text"
-          }*/
+                var json = {title: "", release: "", rating: ""};
 
-        //css3 selector api
-        //"a h1"
-        //" .klasa h1"
-        //#id h1
+                var buf = fs.readFileSync('./config.json');
+                this.title = JSON.parse(buf.toString()).title;
+                this.release = JSON.parse(buf.toString()).release;
+                this.rating = JSON.parse(buf.toString()).rating;
 
-        var buf = fs.readFileSync('./config.json');
-        this.title = JSON.parse(buf.toString()).title;
-        this.release = JSON.parse(buf.toString()).release;
-        this.rating = JSON.parse(buf.toString()).rating;
+                $(this.title).filter(function () { //tytuł bez roku
+                    var data = $(this);
+                    json.title = data.text().split("(")[0];
+                });
 
-        $(this.title).filter(function () { //tytuł bez roku
-            var data = $(this);
-            json.title = data.text().split("(")[0];
+                $(this.release).filter(function () { //rok
+                    var data = $(this);
+                    json.release = data.text();
+                });
+
+                $(this.rating).filter(function () { //ratind
+                    var data = $(this);
+                    json.rating = data.text();
+                });
+
+                res.end(JSON.stringify(json));
+                console.log(json);
+            }
         });
+    });
+}
 
-        $(this.release).filter(function () { //rok
-            var data = $(this);
-            json.release = data.text();
+function getActor(req, res) {
+    var json = '';
+
+    req.on('data', function (data) {
+        json += data;
+        if (json.length > 1e6)
+            request.connection.destroy();
+    });
+
+    req.on('end', function () {
+        var url = JSON.parse(json).URL;
+        request(url, function (error, response, html) {
+            if (!error) {
+                var $ = cheerio.load(html); //sparsowany html w drzewo
+
+                var json = {title: "", release: "", rating: ""};
+
+                var buf = fs.readFileSync('./config.json');
+                this.title = JSON.parse(buf.toString()).title;
+                this.release = JSON.parse(buf.toString()).release;
+                this.rating = JSON.parse(buf.toString()).rating;
+
+                $(this.title).filter(function () { //tytuł bez roku
+                    var data = $(this);
+                    json.title = data.text().split("(")[0];
+                });
+
+                res.end(JSON.stringify({"actor":"actorka"}));
+                console.log(json);
+            }
         });
-
-        $(this.rating).filter(function () { //ratind
-            var data = $(this);
-            json.rating = data.text();
-        });
+    });
+}
 
 
-        console.log(json);
+function requestHandler(request, response) {
+    switch (request.method) {
+        case 'GET':
+            if (request.url.toString() === '/movie') {
+                getMovie(request, response);
+            }
+            else if(request.url.toString() === '/actor'){
+                getActor(request, response);
+            }
+            else {
+                console.log("hwdp");
+            }
+            break;
+
+        default:
+            console.log("METODA NIE OBSŁUŻONA");
     }
+}
+
+var server = http.createServer(requestHandler);
+
+server.listen(3000, function () {
+    console.log("server started");
 });
